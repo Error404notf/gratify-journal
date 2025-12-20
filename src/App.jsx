60b6ddc55ecb411c91e4527f3e969e26ddd3d382
Neon Muthoni <neonmuthoni2@gmail.com>
2025-12-20 23:12:51 +0300

    modifies app.jsx file : removes commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router";
-// import Navbar from "./components/Navbar";
-
-// import LandingPage from "./pages/LandingPage";
-// import JournalPage from "./pages/JournalPage";
-// import SavedAffirmationsPage from "./pages/SavedAffirmationsPage";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/journal" element={<JournalPage />} />
-  
-//         <Route path="/saved-affirmations" element={<SavedAffirmationsPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 import LandingPage from "./pages/LandingPage";
